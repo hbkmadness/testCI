d8594218bdacc64b98eb7d2a8948ab93076d33a4
Slavi Kalov <skalov@melontech.com>
2018-09-27 15:48:27 +0300

    circleci

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", "tab"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
+};
